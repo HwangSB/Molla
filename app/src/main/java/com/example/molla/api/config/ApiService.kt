4aad1b57fb04455412fc12347ad7ab688c03765c
ParkHyunS00 <parkhyuns00@naver.com>
2024-09-26 19:25:47 +0900

    [feat] 다이어리 api 개발중

--- app/src/main/java/com/example/molla/api/config/ApiService.kt
+++ app/src/main/java/com/example/molla/api/config/ApiService.kt
@@ -3,7 +3,6 @@ package com.example.molla.api.config
 import com.example.molla.api.dto.request.DiaryCreateRequest
 import com.example.molla.api.dto.request.DiaryUpdateRequest
 import com.example.molla.api.dto.request.LoginRequest
-import com.example.molla.api.dto.response.DiaryCreateResponse
 import com.example.molla.api.dto.response.DiaryDeleteResponse
 import com.example.molla.api.dto.response.DiaryResponse
 import com.example.molla.api.dto.response.DiaryUpdateResponse
